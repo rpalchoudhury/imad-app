0ccdb2f88af698f8c30f0cff1502c19272a729fd
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-03 17:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,15 @@ element.innerHTML="My Name Is Rituparna";
 
 //Move the image onClick
 var img=document.getElementById("madi");
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    
+    //img.style.marginLeft='100px';//This will make it jump to the right becoz of 100px margin inserted to the left of the image
+    //Below code to move the image gradually to right, i.e. animation
+    var interval=setInterval(moveRight,100);//every 100 ms move the image to the right
 };
