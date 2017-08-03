10af23df431921519c823310545f5f63e4b2b685
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-03 16:39:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ function exampleDemo(data)
 </html>`;
 return htmlTemplate;
 }
+//Here we are sending a text to response object, res, not contents of any file
+app.get('/mypage1', function (req, res) {
+  res.send('This is article four, sending just plain text to response object as a String through res.send(...)');
+});
+
+app.get('/mypage2', function (req, res) {
+  res.send(exampleDemo(obj));
+});
 
 var articles={
  'article-one':{
