b02039639d1b3e3faf1de827a0e967878a552f15
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-03 12:02:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');//express is the library imported to create web servers,handle http connections,listening //on ports
+var morgan = require('morgan');//morgan is the library imported to help us output logs of the server, what requests coming to the server and how we //are responding
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
+//if this url is requested, i.e. if user uses a slash, this function will execute, function tells pick up index.html and send contents of that index.html to response object res
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+//Here we are sending a text to response object, res, not contents of any file
 app.get('/article-one', function (req, res) {
   res.send('This is article one');
 });
@@ -21,10 +23,12 @@ app.get('/article-three', function (req, res) {
   res.send('This is article three');
 });
 
+//if style.css is requested,this function will execute,contents of style.css is sent to response( referenced as res here) object
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+//if the following url path /ui/madi.png is requested, this function will execute
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
