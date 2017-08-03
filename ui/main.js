42e152d6038a56eaff752a78d68a9b3068d45019
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-03 17:42:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,5 +14,5 @@ img.onclick=function(){
     
     //img.style.marginLeft='100px';//This will make it jump to the right becoz of 100px margin inserted to the left of the image
     //Below code to move the image gradually to right, i.e. animation
-    var interval=setInterval(moveRight,100);//every 100 ms move the image to the right
+    var interval=setInterval(moveRight,50);//every 100ms move d img to d right,u can decreasse it to 50 to make it more gradual 
 };
