3a129036e965fac52268f0df39d3f32caacb87f8
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-03 16:45:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ function exampleDemo(data)
 </html>`;
 return htmlTemplate;
 }
+/*
 //Here we are sending a text to response object, res, not contents of any file
 app.get('/mypage1', function (req, res) {
   res.send('This is article four, sending just plain text to response object as a String through res.send(...)');
@@ -46,7 +47,7 @@ app.get('/mypage1', function (req, res) {
 app.get('/mypage2', function (req, res) {
   res.send(exampleDemo(obj));
 });
-
+*/
 var articles={
  'article-one':{
     title:'SSH Facts By Rituparna Palchoudhury',
@@ -146,7 +147,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-/*
+
 //Here we are sending a text to response object, res, not contents of any file
 app.get('/mypage1', function (req, res) {
   res.send('This is article four, sending just plain text to response object as a String through res.send(...)');
@@ -155,7 +156,7 @@ app.get('/mypage1', function (req, res) {
 app.get('/mypage2', function (req, res) {
   res.send(exampleDemo(obj));
 });
-*/
+
 //if style.css is requested,this function will execute,contents of style.css is sent to response( referenced as res here) object
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
