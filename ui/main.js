4eee19eef8b7bad0b30936cc9d05568200a0de81
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-09 17:10:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var onclick=function(){
     }
     console.log('Outside if else Clicked!'+obj.value);
 };
-var onblur=function(){
+var onfocusout=function(){
     if(document.getElementById('commentinput').value==='')
     {
         console.log('Inside if else blurred!');
