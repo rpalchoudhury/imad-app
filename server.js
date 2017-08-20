92b4c0057f1ebfa208d056c55a8bbf40c52859b8
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-20 23:41:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -282,7 +282,12 @@ app.get('/articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM app_article WHERE urlpathkeyword=$1",[req.params.articleName],function(err,result){
         if(err){ res.status(500).send(err.toString()); }
         else if(result.rows.length===0){  res.status(404).send('Article Not Found'); }
-        else { var articleData=result.rows[0]; res.send(createTemplate(articleData,commentobj)); }
+        else 
+        { 
+            var articleData=result.rows[0]; 
+            res.send(createTemplate(articleData,commentobj)); 
+            
+        }
     });
     
 });
