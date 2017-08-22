896244b92a8dc5ddf751de6690bff5646c6a5133
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-22 14:12:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,10 @@
 console.log('Loaded!');
-var submitButton=document.getElementById('submit');
- var nameList=document.getElementById('namelist');
-submitButton.onclick=function(){
-    
-    //Create the request object
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState==XMLHttpRequest.DONE)
-        {
-            if(request.status==200)
-            {
-                //Capture a list of names (along with the new name entered appended) and render it as a list
-                //var names=['name1','name2','name3','name4'];
-                var names=request.responseText;
-                names=JSON.parse(names);//converting the string obtained above back to an object, in this case an array object
-                var list='';
-                for(var i=0;i<names.length;i++)
-                    {
-       
-                     list +='<li>'+names[i]+'</li>';
-         
-                    }
-   
-            nameList.innerHTML=list;
-            }
-        }
-    
-    };    
-    
-//Make a request to the server and send the name 
-var inputText=document.getElementById('name').value;
-request.open("GET","http://rpalchoudhury50.imad.hasura-app.io/submit-name?name="+inputText,true);//this url shud match endpoint /submit-name in //server.js
-request.send(null);
-   
-    }; 
+var loggedField=document.getElementById('logged');
+
+
     
     //XMLHttpRequest For Login Endpoint
     var login=document.getElementById('login');
-    var loggedfield=document.getElementById('logged');
     login.onclick=function(){
         console.log('inside onclick login');
         var request=new XMLHttpRequest();
@@ -94,6 +61,34 @@ request.send(null);
         request.send(JSON.stringify({username: username, password: password}));
     };
     
+    //XMLHttpRequest For Logout Endpoint
+    var loggedValue=loggedField.value;
+    if(loggedValue==1||loggedValue.equals("1"))
+    {
+    var logout=document.getElementById('logout');
+    logout.onclick=function(){
+        console.log('inside onclick logout');
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function(){
+            if(request.readyState==XMLHttpRequest.DONE)
+            {
+                if(request.status==200)
+                {
+                    loggedField.value="0";
+                    alert('logged out successfully');
+                }
+                else if(request.status==500)
+                {
+                alert('something went wrong on the server,please logout again,sorry for inconvenience');
+                }
+            }
+            
+        };
+        //Make the request for endpoint
+        request.open("GET","http://rpalchoudhury50.imad.hasura-app.io/logout",true);
+        request.send(null);
+    };
+    }
     
 
 
