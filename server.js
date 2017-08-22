23390e2ec24cbd51adad0818b7763428c8ac15c0
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-22 19:43:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/create-user',function(req,res){
         if(err){
             res.status(500).send(err.toString());
         }else{
-            res.send("User successfully created:"+dbUsername);
+            res.send("User successfully created:"+dbUserName);
         }
     });
 });
