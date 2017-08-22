e2fef056555b6175a2a39b9cd4f75bd8e1356c7e
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-22 12:56:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@ request.send(null);
         };
         var username=document.getElementById('username').value;
         var password=document.getElementById('password').value;
-        console.log(username);console.log(password);
+        //console.log(username);console.log(password);
         request.open("POST","http://rpalchoudhury50.imad.hasura-app.io/login",true);
         request.setRequestHeader('Content-Type','application/json');
         request.send(JSON.stringify({username: username, password: password}));
