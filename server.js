6d5d544d6696880ee7f2a0b7ad1781fa867b3fab
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-08-28 16:06:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 //create the pool somewhere globally so its lifetime lasts for as long as your app is running
 var pool=new Pool(config);
 
-var array=[];var temparray;var counting=0;
+var array=[];var temparray;var counting=0;var uname="";
 app.get('/get-articles',function(req,res){
     pool.query('SELECT * FROM app_article',function(err,result){
     if(err)
@@ -48,11 +48,11 @@ app.get('/get-articles',function(req,res){
         {
             pool.query('SELECT username FROM user WHERE id=result.row[i].id',function(err,result){
                
-                {var name=result.rows[0].username;}
+                {uname=result.rows[0].username;}
             });
             var id=result.rows[i].id;var title=result.rows[i].title;var heading=result.rows[i].heading;
             var date=result.rows[i].date; var content=result.rows[i].content;
-            temparray={ "id":id, "title":title, "heading":heading, "date":date, "content":content, "name":name };
+            temparray={ "id":id, "title":title, "heading":heading, "date":date, "content":content, "name":uname };
             if(counting===0)array.push(temparray);
         }    
         res.send(JSON.stringify(array));counting=1;
