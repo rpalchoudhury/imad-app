27d2d4f77393b782547c3105086eb0bbafcf4c0f
rpalchoudhury <30633108+rpalchoudhury@users.noreply.github.com>
2017-09-05 23:17:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,6 @@ app.post('/generate_checksum',function(req,res){
                         console.log('successfully generated checksum=>'+checksumhashobj+",chechsum=>"+checksumhash);
 						//res.writeHead(200, {'Content-type' : 'text/json','Cache-Control': 'no-cache'});
 						res.status(200).send(JSON.stringify(checksumhashobj));
-						res.end();
                              }
 					});
 });
